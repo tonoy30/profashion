f41ef73f496d0e3a21dd7b21f42dd4d3a272cb74
tonoy <tonoy.sust@gmail.com>
2020-08-11 02:02:13 +0600

    hoday

--- startup.bat
+++ startup.bat
@@ -4,5 +4,5 @@ start cmd /k "dotnet run --project D:\Programming\C#\profashion\src\profashion.a
 :: Strating profashion activity api
 start cmd /k "dotnet run --project D:\Programming\C#\profashion\src\profashion.services.activities\profashion.services.activities.csproj"
 
-:: profashion.services.identity
+:: profashion services identity
 start cmd /k "dotnet run --project D:\Programming\C#\profashion\src\profashion.services.identity\profashion.services.identity.csproj"
